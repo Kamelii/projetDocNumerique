6656ca4840984908de9ce31b22086ac100c020f5
qbiss <qbiss@161.3.55.190>
2018-11-28 11:01:43 +0100

    Interfaces

--- src/main/java/com/mycompany/interfacegraphique/InterfaceAut.java
+++ src/main/java/com/mycompany/interfacegraphique/InterfaceAut.java
@@ -5,16 +5,21 @@
  */
 package com.mycompany.interfacegraphique;
 
+import com.company.tools.XmlTools;
 import java.awt.Color;
+import java.io.StringWriter;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamWriter;
 
 /**
  *
  * @author DELL XPS
  */
 public class InterfaceAut extends java.awt.Frame {
-    private String select = "Jour(s)";
+  static String em;
     /**
      * Creates new form InterfaceAut
      */
@@ -34,25 +39,31 @@ public class InterfaceAut extends java.awt.Frame {
         pan = new javax.swing.JPanel();
         mailEmetteur = new javax.swing.JLabel();
         nomEmetteur = new javax.swing.JLabel();
-        choixDuree = new javax.swing.JComboBox<>();
         titreFenetreProp = new javax.swing.JLabel();
         nomRecepteur = new javax.swing.JLabel();
-        texteDuree = new javax.swing.JTextField();
         texteMailEmetteur = new javax.swing.JTextField();
         duree = new javax.swing.JLabel();
-        mailRecepteur = new javax.swing.JLabel();
+        descDemande = new javax.swing.JLabel();
         texteEmetteur = new javax.swing.JTextField();
         textMailRecepteur = new javax.swing.JTextField();
         texteRecepteur = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        boutonEnvoyerDemande = new javax.swing.JButton();
+        mailRecepteur1 = new javax.swing.JLabel();
+        scrollDescObjPropose = new javax.swing.JScrollPane();
+        texteDescDemande = new javax.swing.JTextPane();
+        choixNbJour = new javax.swing.JSpinner();
+        jLabel1 = new javax.swing.JLabel();
 
-        setPreferredSize(new java.awt.Dimension(535, 370));
+        setLocationRelativeTo(null);
+        setMinimumSize(new java.awt.Dimension(520, 330));
+        setPreferredSize(new java.awt.Dimension(535, 490));
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 exitForm(evt);
             }
         });
 
+        pan.setPreferredSize(new java.awt.Dimension(540, 320));
         pan.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         mailEmetteur.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
@@ -64,15 +75,6 @@ public class InterfaceAut extends java.awt.Frame {
         nomEmetteur.setText("Nom émetteur : *");
         nomEmetteur.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
         pan.add(nomEmetteur, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 80, 160, 20));
-
-        choixDuree.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        choixDuree.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Jour(s)", "Semaine(s)", "Mois", "Année(s)", "Durée Indéterminée" }));
-        choixDuree.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                choixDureeActionPerformed(evt);
-            }
-        });
-        pan.add(choixDuree, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 160, -1, 20));
 
         titreFenetreProp.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
         titreFenetreProp.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -86,10 +88,8 @@ public class InterfaceAut extends java.awt.Frame {
         nomRecepteur.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
         pan.add(nomRecepteur, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 120, 160, 20));
 
-        texteDuree.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        pan.add(texteDuree, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 160, 60, 20));
-
         texteMailEmetteur.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        texteMailEmetteur.setNextFocusableComponent(textMailRecepteur);
         pan.add(texteMailEmetteur, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 200, 330, 20));
 
         duree.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
@@ -97,29 +97,59 @@ public class InterfaceAut extends java.awt.Frame {
         duree.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
         pan.add(duree, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 160, 160, 20));
 
-        mailRecepteur.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        mailRecepteur.setText("Mail récepteur : ");
-        mailRecepteur.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
-        pan.add(mailRecepteur, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 240, 160, -1));
+        descDemande.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        descDemande.setText("Description :");
+        descDemande.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+        pan.add(descDemande, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 280, 160, -1));
 
         texteEmetteur.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        texteEmetteur.setNextFocusableComponent(texteRecepteur);
         pan.add(texteEmetteur, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 80, 330, 20));
 
         textMailRecepteur.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        textMailRecepteur.setNextFocusableComponent(texteDescDemande);
         pan.add(textMailRecepteur, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 240, 330, -1));
 
         texteRecepteur.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        texteRecepteur.setNextFocusableComponent(choixNbJour);
         pan.add(texteRecepteur, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 120, 330, 20));
 
-        jButton1.setBackground(new java.awt.Color(102, 204, 255));
-        jButton1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
-        jButton1.setText("Envoyer la demande d'autorisation");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        boutonEnvoyerDemande.setBackground(new java.awt.Color(102, 204, 255));
+        boutonEnvoyerDemande.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        boutonEnvoyerDemande.setText("Envoyer la demande d'autorisation");
+        boutonEnvoyerDemande.setNextFocusableComponent(texteEmetteur);
+        boutonEnvoyerDemande.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                boutonEnvoyerDemandeActionPerformed(evt);
             }
         });
-        pan.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 280, 370, 40));
+        pan.add(boutonEnvoyerDemande, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 400, 370, 40));
+
+        mailRecepteur1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        mailRecepteur1.setText("Mail récepteur : ");
+        mailRecepteur1.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+        pan.add(mailRecepteur1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 240, 160, -1));
+
+        texteDescDemande.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        texteDescDemande.setAutoscrolls(false);
+        texteDescDemande.setNextFocusableComponent(boutonEnvoyerDemande);
+        scrollDescObjPropose.setViewportView(texteDescDemande);
+
+        pan.add(scrollDescObjPropose, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 280, 330, 100));
+
+        choixNbJour.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        choixNbJour.setNextFocusableComponent(texteMailEmetteur);
+        choixNbJour.setValue(1);
+        choixNbJour.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                choixNbJourStateChanged(evt);
+            }
+        });
+        pan.add(choixNbJour, new org.netbeans.lib.awtextra.AbsoluteConstraints(181, 160, 50, -1));
+
+        jLabel1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel1.setText("Jour(s)");
+        pan.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 150, -1, 40));
 
         add(pan, java.awt.BorderLayout.CENTER);
 
@@ -133,31 +163,17 @@ public class InterfaceAut extends java.awt.Frame {
         System.exit(0);
     }//GEN-LAST:event_exitForm
 
-    private void choixDureeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_choixDureeActionPerformed
-        JComboBox cb = (JComboBox) evt.getSource();
-
-        select = cb.getSelectedItem().toString();
-
-        if(select.compareTo("Durée Indéterminée") == 0){
-            texteDuree.setFocusable(false);
-            texteDuree.setBackground(new Color(230,230,230));
-
-            pan.repaint();
-        }
-        else{
-            texteDuree.setFocusable(true);
-            texteDuree.setBackground(new Color(255,255,255));
-
-            pan.repaint();
-        }
-    }//GEN-LAST:event_choixDureeActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void boutonEnvoyerDemandeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_boutonEnvoyerDemandeActionPerformed
         int err = 0;
+        
         
         if(texteEmetteur.getText().isEmpty()){
             JOptionPane.showMessageDialog(null,"Le champs \"Nom émetteur\" doit être rempli!");
             err = 1;
+        }
+        else {
+       em = texteEmetteur.getText();
+        
         }
         
         if(texteRecepteur.getText().isEmpty()){
@@ -165,50 +181,54 @@ public class InterfaceAut extends java.awt.Frame {
             err = 1;
         }
         
-        if(select.compareTo("Durée Indéterminée") != 0){
-            if(texteDuree.getText().isEmpty()){
-                if(select.compareTo("Année(s)") == 0){
-                    JOptionPane.showMessageDialog(null,"Vous devez spécifier un nombre d'Année(s)!");
-                    err = 1;
-                }
-                else{
-                   JOptionPane.showMessageDialog(null,"Vous devez spécifier un nombre de " + select + "!");
-                   err = 1;
-                }
-            }
-            
-            if(!texteDuree.getText().matches("[0-9]*")){
-                JOptionPane.showMessageDialog(null,"Vous devez entrer un enier pour spécifier la durée!");
-            }
-        }
-        
+     
         if(err != 1){
-            //TODO Mettre la fonction qui crée le fichier xml de l'autorisation
-        }
-    }//GEN-LAST:event_jButton1ActionPerformed
+            XmlTools xmlTools = new XmlTools();
+            if(xmlTools.creerAuth(texteEmetteur.getText(), texteRecepteur.getText(), (Integer)choixNbJour.getValue(),texteMailEmetteur.getText(), textMailRecepteur.getText()))
+                JOptionPane.showMessageDialog(null,"Demande cree avec succes");
+            else
+                JOptionPane.showMessageDialog(null,"Erreur veuillez resseayer plus tard");
+                
+        } 
+    }//GEN-LAST:event_boutonEnvoyerDemandeActionPerformed
 
+    private void choixNbJourStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_choixNbJourStateChanged
+        JSpinner s = (JSpinner) evt.getSource();
+         
+         int value = (Integer) s.getValue();
+         
+         if (value < 1){
+             choixNbJour.setValue(1);
+         }
+    }//GEN-LAST:event_choixNbJourStateChanged
+
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(() -> {
             new InterfaceAut().setVisible(true);
+            
         });
     }
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox<String> choixDuree;
+    private javax.swing.JButton boutonEnvoyerDemande;
+    private javax.swing.JSpinner choixNbJour;
+    private javax.swing.JLabel descDemande;
     private javax.swing.JLabel duree;
-    private javax.swing.JButton jButton1;
     private javax.swing.JFrame jFrame1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel mailEmetteur;
-    private javax.swing.JLabel mailRecepteur;
+    private javax.swing.JLabel mailRecepteur1;
     private javax.swing.JLabel nomEmetteur;
     private javax.swing.JLabel nomRecepteur;
     private javax.swing.JPanel pan;
+    private javax.swing.JScrollPane scrollDescObjPropose;
     private javax.swing.JTextField textMailRecepteur;
-    private javax.swing.JTextField texteDuree;
+    private javax.swing.JTextPane texteDescDemande;
     private javax.swing.JTextField texteEmetteur;
     private javax.swing.JTextField texteMailEmetteur;
     private javax.swing.JTextField texteRecepteur;
